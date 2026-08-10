28c5a54a5b6549fe2ea0f27cb6aa79eb15647c29
Alaa-dev04 <nahea906.a@gmail.com>
2026-08-10 15:08:11 +0300

    Revert "fix: producttoolbar prop types"
    This reverts commit 92d2544ceff3650240244ea0a71cff1edd1405ad.

--- FrontEnd/src/components/shared/websiteshared/producttoolbar.tsx
+++ FrontEnd/src/components/shared/websiteshared/producttoolbar.tsx
@@ -1,7 +1,14 @@
 "use client";
 import { Grid2X2, List, SlidersHorizontal } from "lucide-react";
 import { Button } from "@/components/ui/button";
-
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import useProductToolBar from "@/hooks/useProductToolBar";
 const SORT_OPTIONS = [
   { label: "Relevance (default)", value: "relevance" },
   { label: "Price low to high", value: "price-low" },
@@ -11,28 +18,16 @@ const SORT_OPTIONS = [
   { label: "Newest arrivals", value: "newest" },
 ];
 
-type ProductToolbarProps = {
-  sort: string;
-  setSort: (value: string) => void;
-  view: string;
-  setView: (value: string) => void;
-  onFilterClick?: () => void;
-};
 
-const Producttoolbar = ({
-  sort,
-  setSort,
-  view,
-  setView,
-  onFilterClick,
-}: ProductToolbarProps) => {
+const Producttoolbar = ({ onFilterClick }: { onFilterClick?: () => void }) => {
+    const { sort, setSort, view, setView}= useProductToolBar();
   return (
     <div className="flex items-center gap-3 ">
       <Button variant="outline" onClick={onFilterClick} className="py-5">
-        <SlidersHorizontal size={30} className="mr-1.5" />
+        <SlidersHorizontal size={30} className="mr-1.5  " />
         Filter
       </Button>
-      <select
+       <select
         value={sort}
         onChange={(e) => setSort(e.target.value)}
         className="rounded-md border px-3 py-2 text-sm text-muted-foreground"
@@ -43,22 +38,22 @@ const Producttoolbar = ({
           </option>
         ))}
       </select>
-      <div className="flex items-center rounded-md border overflow-hidden">
-        <button
+       <div className="flex items-center rounded-md border overflow-hidden">
+         <button
           onClick={() => setView("grid")}
           className={`p-1.5 ${view === "grid" ? "bg-gray-100" : ""}`}
         >
           <Grid2X2 size={25} />
         </button>
-        <button
+          <button
           onClick={() => setView("list")}
           className={`p-1.5 ${view === "list" ? "bg-gray-100" : ""}`}
         >
           <List size={25} />
         </button>
-      </div>
+       </div>
     </div>
   );
-};
+}
 
 export default Producttoolbar;
